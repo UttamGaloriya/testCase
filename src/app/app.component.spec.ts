9b532384abe2dd9368f87af89402ed0931d9908b
Uttam Galoriya <19ce037@gardividyapith.ac.in>
2023-08-10 11:40:40 +0530

    testCase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,7 +29,11 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('testCase app is running!');
   });
+
+  test('add two number', () => {
+    console.log("run")
+  })
 });
